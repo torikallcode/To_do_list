5663715b7e24cff0c95a6c8deb21b1b950dc1c3c
torikallcode <torikalcontent@gmail.com>
2025-02-06 21:23:26 +0800

    create list_handler: GetList

--- backend/handlers/list_handler.go
+++ backend/handlers/list_handler.go
@@ -4,6 +4,9 @@ import (
 	"backend/models"
 	"encoding/json"
 	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
 var lists []models.List
@@ -11,4 +14,21 @@ var lists []models.List
 func GetAllList(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(lists)
+}
+
+func GetList(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	id, err := strconv.Atoi(params["id"])
+	if err != nil {
+		http.Error(w, "invalid user", http.StatusBadRequest)
+		return
+	}
+	for _, item := range lists {
+		if item.ID == id {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+	http.Error(w, "user not found", http.StatusNotFound)
 }
