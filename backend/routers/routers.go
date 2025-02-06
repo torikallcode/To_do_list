a28f436c2186638502f4385b87f8d7763e7c9ef5
torikallcode <torikalcontent@gmail.com>
2025-02-06 21:37:31 +0800

    create routers: routers

---
+++ backend/routers/routers.go
@@ -0,0 +1,19 @@
+package routers
+
+import (
+	"backend/handlers"
+
+	"github.com/gorilla/mux"
+)
+
+func SetupRouter() *mux.Router {
+	routers := mux.NewRouter()
+
+	routers.HandleFunc("/list", handlers.GetAllList).Methods("GET")
+	routers.HandleFunc("/list/{id}", handlers.GetList).Methods("GET")
+	routers.HandleFunc("/list", handlers.CreateList).Methods("POST")
+	routers.HandleFunc("/list/{id}", handlers.UpdateList).Methods("PUT")
+	routers.HandleFunc("/list/{id}", handlers.DeleteList).Methods("DELETE")
+
+	return routers
+}
