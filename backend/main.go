4d592f5add41c24209ac3159138037581a2da174
torikallcode <torikalcontent@gmail.com>
2025-02-06 21:37:58 +0800

    create main.go

---
+++ backend/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"backend/routers"
+	"log"
+	"net/http"
+
+	"github.com/rs/cors"
+)
+
+func main() {
+	router := routers.SetupRouter()
+
+	c := cors.New(cors.Options{
+		AllowedOrigins: []string{"http://localhost:5173"},
+		AllowedMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders: []string{"Content-Type", "Authorization"},
+		Debug:          true,
+	})
+
+	handler := c.Handler(router)
+
+	log.Println("Server sedang berjalan di port :8080...")
+	log.Fatal(http.ListenAndServe(":8080", handler))
+}
