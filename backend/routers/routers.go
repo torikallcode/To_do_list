1a38bff89250d5c333e818dce0b1aadc8e19d694
torikallcode <torikalcontent@gmail.com>
2025-02-08 09:34:00 +0800

    create router: UpdateListStatus

--- backend/routers/routers.go
+++ backend/routers/routers.go
@@ -14,6 +14,8 @@ func SetupRouter() *mux.Router {
 	routers.HandleFunc("/list", handlers.CreateList).Methods("POST")
 	routers.HandleFunc("/list/{id}", handlers.UpdateList).Methods("PUT")
 	routers.HandleFunc("/list/{id}", handlers.DeleteList).Methods("DELETE")
+	// Di main.go atau router setup
+	routers.HandleFunc("/list/{id}/status", handlers.UpdateListStatus).Methods("PATCH")
 
 	return routers
 }
