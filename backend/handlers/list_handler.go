f2a76581c72c1c4e3c66b43d0a15fe427cf2164c
torikallcode <torikalcontent@gmail.com>
2025-02-20 19:52:58 +0800

    create handler GetAllList with eplain

--- backend/handlers/list_handler.go
+++ backend/handlers/list_handler.go
@@ -12,40 +12,36 @@ import (
 )
 
 func GetAllList(w http.ResponseWriter, r *http.Request) {
-	// set header response sebaga json
+	// Set header response sebagai JSON
+	// Eksekusi query SELECT untuk mengambil semua data
+	// Membaca hasil query baris per baris
+	// Menyimpan setiap baris ke dalam slice lists
+	// Mengirim seluruh data lists sebagai JSON response
+
 	w.Header().Set("Content-Type", "application/json")
 
 	var lists []models.List
 
-	// membuat kode query yang akan dieksekusi
 	query := "SELECT id, name_list, status FROM lists"
-
-	// digunakan untuk mengeksekusi query ke database. (database.DB adalah tempat koneksi databasenya)
 	rows, err := database.DB.Query(query)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	//menutup cursor yang digunakan untuk mengakses hasil query (setelah fungsi selesai)
-	defer rows.Close()
-
-	// membaca baris hasil query satu per satu
 	for rows.Next() {
 		var list models.List
-		// memindahkan data dari baris saat ini(di database) ke variable list yang sudah dibuat
 		if err := rows.Scan(&list.ID, &list.Name_list, &list.Status); err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
-		// menambahkan baris saat ini ke slice lists
 		lists = append(lists, list)
 	}
-	// ubah format slice licsts ke json dan kirim sebagai response
 	json.NewEncoder(w).Encode(lists)
 }
 
 func GetList(w http.ResponseWriter, r *http.Request) {
+
 	// Set header sebagi json
 	w.Header().Set("Content-Type", "application/json")
 	// ambil parameter url (id)
